1806d89b6e6af429d8f4f716ae5a1b9fbc75675c
CHOIJUNHYUK <hazard0448@gmail.com>
2025-05-01 21:25:50 +0900

    feat: starter

---
+++ starter/starterApp.swift
@@ -0,0 +1,17 @@
+//
+//  starterApp.swift
+//  starter
+//
+//  Created by CHOIJUNHYUK on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct starterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
